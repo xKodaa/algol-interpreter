731ef56b181e3a8e1b115256cc16b68aa893a342
xKodaa <jakub.kodytek@email.cz>
2024-01-28 16:49:58 +0100

    Přidána main struktura

--- Algol-interpreter/Program.cs
+++ Algol-interpreter/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Algol_interpreter.Grammar;
+using Algol_interpreter.Visitor;
+using Antlr4.Runtime;
+
+var fileName = "priklad1.txt";
+
+var fileContents = File.ReadAllText(fileName);
+
+var inputStream = new AntlrInputStream(fileContents);
+var algolLexer = new Algol60Lexer(inputStream);
+var commonTokenStream = new CommonTokenStream(algolLexer);
+var algolParser = new Algol60Parser(commonTokenStream);
+
+var algolContext = algolParser.program();
+var visitor = new AlgolVisitor();
+
+visitor.Visit(algolContext);
+
+Console.ReadKey();
